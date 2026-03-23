3c58c9e3823d5d42a977cdf159b6f8e71701c51e
wasseflouati <109959970+wasseflouati@users.noreply.github.com>
2026-03-23 14:05:55 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
-echo "Hello World, version 2";
+
+echo "Hello World, version 3<br>";
+
+$con = mysqli_init();
+
+if (!mysqli_real_connect(
+    $con,
+    "hello-php-bd-fsegs-server.mysql.database.azure.com",
+    "boahhaiygn",
+    'nCv0EyM$eJzNRO5q',
+    "hello-php-bd-fsegs-database",
+    3306
+)) {
+    die("Connexion échouée : " . mysqli_connect_error());
+}
+
+echo "Connexion à la base de données réussie<br>";
 ?>
