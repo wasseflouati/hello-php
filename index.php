c6968428e0a098f95c9533d8739a599762d4c1fd
wasseflouati <109959970+wasseflouati@users.noreply.github.com>
2026-03-23 12:33:46 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,27 @@
 <?php
-echo "Hello World, version 29";
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+echo "Hello World, version 30<br>";
+
 $con = mysqli_init();
 $sslCa = __DIR__ . '/DigiCertGlobalRootCA.crt.pem';
-mysqli_ssl_set($con,NULL,NULL,$sslCa, NULL, NULL);
-if (! mysqli_real_connect($conn, "hbd-server.mysql.database.azure.com", "zgnrykaexx", "{kQRcUNiXHn$f0O3Gl}", "{hbd-database}", 3306, MYSQLI_CLIENT_SSL))
-{
+
+mysqli_ssl_set($con, NULL, NULL, $sslCa, NULL, NULL);
+
+if (!mysqli_real_connect(
+    $con,
+    "hbd-server.mysql.database.azure.com",
+    "zgnrykaexx",
+    "QRcUNiXHn$f0O3Gl",
+    "hbd-database",
+    3306,
+    NULL,
+    MYSQLI_CLIENT_SSL
+)) {
     die("Connexion échouée : " . mysqli_connect_error());
 }
-echo "Hello World, version 3";
+
+echo "Hello World, version 3<br>";
+echo "Connexion réussie";
 ?>
