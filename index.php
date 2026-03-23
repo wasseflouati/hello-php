47bf120501152d0e9682ae0003c4488f2816f0fd
wasseflouati <109959970+wasseflouati@users.noreply.github.com>
2026-03-23 11:45:09 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-echo "Hello World, version 22";
+echo "Hello World, version 23";
 $con = mysqli_init();
 mysqli_ssl_set($con,NULL,NULL, "{MysqlflexGlobalRootCA.crt.pem}", NULL, NULL);
 mysqli_real_connect($conn, "hbd-server.mysql.database.azure.com", "zgnrykaexx", "{QRcUNiXHn$f0O3Gl}", "{hbd-database}", 3306, MYSQLI_CLIENT_SSL);
