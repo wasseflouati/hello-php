4f86c2f344a91d588e22169c34c1615f3c5f952c
wasseflouati <109959970+wasseflouati@users.noreply.github.com>
2026-03-23 11:19:45 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php
 echo "Hello World, version 2";
+$con = mysqli_init();
+mysqli_ssl_set($con,NULL,NULL, "{path to CA cert}", NULL, NULL);
+mysqli_real_connect($conn, "hbd-server.mysql.database.azure.com", "zgnrykaexx", "{QRcUNiXHn$f0O3Gl}", "{hbd-database}", 3306, MYSQLI_CLIENT_SSL);
+echo "Hello World, version 3";
 ?>
